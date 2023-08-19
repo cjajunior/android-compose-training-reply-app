b53cf4b819262c871028b1030acc594ae4c615be
junior <claudiojunioralicate@gmail.com>
2023-08-18 23:21:09 -0300

    test: Teste para confirmar retenção de email selecionado em mudança de configuração

---
+++ app/src/androidTest/java/com/example/reply/test/ReplyAppStateRestorationTest.kt
@@ -0,0 +1,56 @@
+package com.example.reply.test
+
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.StateRestorationTester
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.example.reply.R.string.navigation_back
+import com.example.reply.data.local.LocalEmailsDataProvider
+import com.example.reply.ui.ReplyApp
+import org.junit.Rule
+import org.junit.Test
+
+class ReplyAppStateRestorationTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun compactDevice_selectedEmailEmailRetained_afterConfigChange() {
+        // Setup compact window
+        val stateRestorationTester = StateRestorationTester(composeTestRule)
+        stateRestorationTester.setContent { ReplyApp(windowSize = WindowWidthSizeClass.Compact) }
+
+        // Given third email is displayed
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)
+        ).assertIsDisplayed()
+
+        // Open detailed page
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].subject)
+        ).performClick()
+
+        // Verify that it shows the detailed screen for the correct email
+        composeTestRule.onNodeWithContentDescriptionForStringId(
+            navigation_back,
+        ).assertExists()
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)
+        ).assertExists()
+
+        // Simulate a config change
+        stateRestorationTester.emulateSavedInstanceStateRestore()
+
+        // Verify that it still shows the detailed screen for the same email
+        composeTestRule.onNodeWithContentDescriptionForStringId(
+            navigation_back,
+        ).assertExists()
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)
+        ).assertExists()
+    }
+
+}
